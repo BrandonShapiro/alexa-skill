c845854455296c3620adef77448f3276eafe993d
Brandon Shapiro <brandonshapiro9@gmail.com>
2022-05-19 21:09:14 -0600

    Assignment 1 - I forgot to push changes before working on Assignment 2, so here's both basically.

--- src/main/java/com/weber/cs3230/ChatBotMain.java
+++ src/main/java/com/weber/cs3230/ChatBotMain.java
@@ -5,52 +5,10 @@ import java.util.Scanner;
 public class ChatBotMain {
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome to ChatBot!");
-        System.out.println();
-        System.out.println("Please ask a question or type 'q' to quit: ");
-
-        Scanner input = new Scanner(System.in);
-        String question = input.nextLine();
-
-        while(!question.equalsIgnoreCase("q")){
-            switch (question){
-                case "what does a dog say?":
-                    System.out.println("Woof!");
-                    break;
-                case "what does a cat say?":
-                    System.out.println("Meowwww");
-                    break;
-                case "what does a pig say?":
-                    System.out.println("Oink Oink!");
-                    break;
-                case "what does a horse say?":
-                    System.out.println("Neighhhh");
-                    break;
-                case "what does a cow say?":
-                    System.out.println("Moooooo");
-                    break;
-                case "what does a chicken say?":
-                    System.out.println("cluck cluck");
-                    break;
-                case "what does a duck say?":
-                    System.out.println("Quack quack!");
-                    break;
-                case "what does a fish say?":
-                    System.out.println("glub glub glub");
-                    break;
-                case "what does a rooster say?":
-                    System.out.println("Cockadoodledoo!");
-                    break;
-                case "what does a sheep say?":
-                    System.out.println("Baaahhhh");
-                    break;
-            }
-
-            System.out.println("Ask another question or type 'q' to quit: ");
-            question = input.nextLine();
-        }
-
+        WelcomeMessage w = new WelcomeMessage();
+        w.printWelcome();
+        ChatBotRunner cb = new ChatBotRunner();
+        cb.StartChatting();
 
     }
 }
---
+++ src/main/java/com/weber/cs3230/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package com.weber.cs3230;
+
+public class WelcomeMessage {
+
+    public static void printWelcome(){
+        System.out.println("-----------------------------------------");
+        System.out.println("|          Welcome to ChatBot!          |");
+        System.out.println("-----------------------------------------");
+        System.out.println();
+        System.out.println("Please ask a question or type 'q' to quit: ");
+    }
+}
