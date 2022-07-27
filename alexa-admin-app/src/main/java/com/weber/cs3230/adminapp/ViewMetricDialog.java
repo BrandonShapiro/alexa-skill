542fa0cd3b528246c7b0181deacab595e18e546e
Brandon Shapiro <brandonshapiro9@gmail.com>
2022-07-26 20:51:37 -0600

    minor changes

--- alexa-admin-app/src/main/java/com/weber/cs3230/adminapp/ViewMetricDialog.java
+++ alexa-admin-app/src/main/java/com/weber/cs3230/adminapp/ViewMetricDialog.java
@@ -1,6 +1,7 @@
 package com.weber.cs3230.adminapp;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class ViewMetricDialog extends JDialog {
@@ -16,12 +17,12 @@ public class ViewMetricDialog extends JDialog {
     private JPanel getViewMetricPanel(){
         JPanel viewMetricPanel = new JPanel(new GridLayout(2,1));
         JLabel placeHolder = new JLabel("THIS IS A PLACEHOLDER!");
+        placeHolder.setHorizontalAlignment(JLabel.CENTER);
+        placeHolder.setBorder(new EmptyBorder(10,10,10,10));
         viewMetricPanel.add(placeHolder);
 
         JButton exitButton = new JButton("CLOSE");
-        exitButton.addActionListener(e->{
-            closeDialog();
-        });
+        exitButton.addActionListener(e-> closeDialog());
         viewMetricPanel.add(exitButton);
 
         return viewMetricPanel;
